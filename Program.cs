b8a907e0210fbb53cfbe7da212e1f8d273e4aca2
Name Martin <antemratin@gmail.com>
2020-02-06 11:18:59 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace deck_o_cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
